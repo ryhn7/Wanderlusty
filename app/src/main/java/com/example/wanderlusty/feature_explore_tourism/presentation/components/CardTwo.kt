80007d708668ea9e79a377f3b0354d2cdb846890
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-10-11 19:09:52 +0700

    little changes

--- app/src/main/java/com/example/wanderlusty/feature_explore_tourism/presentation/components/CardTwo.kt
+++ app/src/main/java/com/example/wanderlusty/feature_explore_tourism/presentation/components/CardTwo.kt
@@ -1,21 +1,16 @@
 package com.example.wanderlusty.feature_explore_tourism.presentation.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
